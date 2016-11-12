8d9f898217e3f7b5223f1c81cffc1ef441d41cbb
abouthiroppy <git@about-hiroppy.com>
2016-11-12 15:06:25 +0900

    test: add tests

---
+++ test/index.js
@@ -0,0 +1,114 @@
+import test from 'ava';
+import {CLIEngine} from 'eslint';
+import baseConfig from '../rules/base';
+import reactConfig from '../rules/react';
+import flowConfig from '../rules/flow';
+
+reactConfig.parser = 'babel-eslint';
+flowConfig.parser = 'babel-eslint';
+
+const baseJs = `
+import hoge from 'hoge';
+const fuga = require('fuga');
+
+const test1 = 1;
+const test2 = '2';
+const obj = {
+  test1: 1,
+  test2: 2
+};
+
+/**
+ * @description check:)
+ */
+function check() {
+  return 'check';
+}
+
+export default class Piyo extends Fuga {
+  constructor() {
+    super();
+
+    this.name = 'name';
+  }
+
+  get myName() {
+    return this.name;
+  }
+}
+`;
+
+const reactJs = `
+import React from 'react';
+
+class View extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      opened: false
+    };
+  }
+
+  render() {
+    return (
+      <div />
+    );
+  }
+}
+const Label = () => (
+  <div>
+    <h1>Label</h1>
+    <button onClick={() => postMyName()} />
+  </div>
+);
+`;
+
+const flowJs = `
+const name: string = 'name';
+
+type User = {
+  name: string
+};
+
+/**
+ * @description fetchUserNames
+ */
+function fetchUserNames(): Promise<Array<string>> {
+  return new Promise((resolve, reject) => {
+    resolve([
+      'hoge',
+      'fuga'
+    ]);
+  });
+}
+`;
+
+function checkFailedRules(config, source) {
+  const cli = new CLIEngine({
+    useEslintrc: false,
+    baseConfig: config
+  });
+
+  const linter = cli.executeOnText(source);
+
+  return linter.results[0];
+}
+
+test('should return no errored at base', (t) => {
+  const result = checkFailedRules(baseConfig, baseJs);
+
+  t.is(result.errorCount, 0);
+});
+
+test('should return no errored at react', (t) => {
+  const result = checkFailedRules(reactConfig, reactJs);
+
+  t.is(result.errorCount, 0);
+});
+
+test('should return no errored at flow', (t) => {
+  const result = checkFailedRules(flowConfig, flowJs);
+
+  t.is(result.errorCount, 0);
+});
